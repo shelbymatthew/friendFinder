fee23984efec13940c56d563fc327008cb5861b0
Shelby Smith <alackreaction@gmail.com>
2018-04-25 12:06:32 -0500

    adding more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,18 +3,60 @@ var friends = [
     {
         name: "Jim",
         photo: "https://vignette.wikia.nocookie.net/theoffice/images/9/9a/Jim.jpg/revision/latest?cb=20170701084550",
-        scores: [4, 4, 2, 4, 1, 5, 5, 2, 1, 3]
+        scores: [5, 2, 4, 2, 2, 2, 4, 3, 5, 3]
     },
     {
         name: "Pam",
         photo: "https://vignette.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesley.jpg/revision/latest?cb=20170701084358",
-        scores: [2, 5, 3, 5, 4, 4, 2, 1, 4, 1] // 0
+        scores: [3, 5, 5, 2, 3, 3, 4, 4, 4, 5]
     },
     {
         name: "Michael",
         photo: "https://vignette.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg/revision/latest?cb=20170701090332",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5] //12
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
+    {
+        name: "Toby",
+        photo: "https://en.wikipedia.org/wiki/File:Toby_Flenderson_promo_picture.jpg",
+        scores: [5, 5, 5, 1, 2, 3, 2, 3, 2, 5]
+    },
+    {
+        name: "Meredith",
+        photo: "https://en.wikipedia.org/wiki/Meredith_Palmer#/media/File:Meredith_Palmer.jpg",
+        scores: [2, 1, 1, 5, 4, 1, 5, 1, 2, 1]
+    },
+    {
+        name: "Dwight",
+        photo: "https://en.wikipedia.org/wiki/Dwight_Schrute#/media/File:Dwight_Schrute.jpg",
+        scores: [5, 3, 4, 1, 1, 1, 3, 1, 2, 3]
+    },
+    {
+        name: "Andy",
+        photo: "https://en.wikipedia.org/wiki/Andy_Bernard#/media/File:Andy_Bernard_photoshot.jpg",
+        scores: [3, 3, 3, 3, 5, 3, 3, 3, 3, 5]
+    },
+    {
+        name: "Kelly",
+        photo: "https://en.wikipedia.org/wiki/Kelly_Kapoor#/media/File:Kelly_Kapoor.jpg",
+        scores: [1, 1, 1, 5, 4, 1, 5, 3, 3, 4]
+    },
+    {
+        name: "Angela",
+        photo: "https://en.wikipedia.org/wiki/Angela_Kinsey#/media/File:Angela_kinsey.jpg",
+        scores: [1, 3, 5, 1, 1, 5, 1, 5, 1, 1]
+    },
+    {
+        name: "Oscar",
+        photo: "https://en.wikipedia.org/wiki/File:Oscar_Martinez_of_The_Office.jpg",
+        scores: [2, 5, 5, 3, 3, 4, 4, 2, 3, 5]
+    },
+    {
+        name: "Kevin",
+        photo: "https://en.wikipedia.org/wiki/File:Office-1200-baumgartner1.jpg",
+        scores: [1, 1, 2, 5, 5, 1, 5, 1, 5, 5]
+    },
+
+
 
 ];
 
