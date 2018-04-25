ee6510bc93857fecc609ff77f61f71841ecf879a
Shelby Smith <alackreaction@gmail.com>
2018-04-25 14:22:59 -0500

    minor fixes, cleaned up code.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,6 @@ app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
-
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
 });
