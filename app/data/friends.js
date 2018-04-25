60b5789c119dd3e978a5c3cf250ee98f5aa1a49b
Shelby Smith <alackreaction@gmail.com>
2018-04-25 12:17:26 -0500

    fixing photo links

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,42 +17,42 @@ var friends = [
     },
     {
         name: "Toby",
-        photo: "https://en.wikipedia.org/wiki/File:Toby_Flenderson_promo_picture.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/1/18/Toby_Flenderson_promo_picture.jpg",
         scores: [5, 5, 5, 1, 2, 3, 2, 3, 2, 5]
     },
     {
         name: "Meredith",
-        photo: "https://en.wikipedia.org/wiki/Meredith_Palmer#/media/File:Meredith_Palmer.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/6/6f/Meredith_Palmer.jpg",
         scores: [2, 1, 1, 5, 4, 1, 5, 1, 2, 1]
     },
     {
         name: "Dwight",
-        photo: "https://en.wikipedia.org/wiki/Dwight_Schrute#/media/File:Dwight_Schrute.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg",
         scores: [5, 3, 4, 1, 1, 1, 3, 1, 2, 3]
     },
     {
         name: "Andy",
-        photo: "https://en.wikipedia.org/wiki/Andy_Bernard#/media/File:Andy_Bernard_photoshot.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/8/84/Andy_Bernard_photoshot.jpg",
         scores: [3, 3, 3, 3, 5, 3, 3, 3, 3, 5]
     },
     {
         name: "Kelly",
-        photo: "https://en.wikipedia.org/wiki/Kelly_Kapoor#/media/File:Kelly_Kapoor.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/6/69/Kelly_Kapoor.jpg",
         scores: [1, 1, 1, 5, 4, 1, 5, 3, 3, 4]
     },
     {
         name: "Angela",
-        photo: "https://en.wikipedia.org/wiki/Angela_Kinsey#/media/File:Angela_kinsey.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/8/8c/Angela_kinsey.jpg",
         scores: [1, 3, 5, 1, 1, 5, 1, 5, 1, 1]
     },
     {
         name: "Oscar",
-        photo: "https://en.wikipedia.org/wiki/File:Oscar_Martinez_of_The_Office.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/5/54/Oscar_Martinez_of_The_Office.jpg",
         scores: [2, 5, 5, 3, 3, 4, 4, 2, 3, 5]
     },
     {
         name: "Kevin",
-        photo: "https://en.wikipedia.org/wiki/File:Office-1200-baumgartner1.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/en/6/60/Office-1200-baumgartner1.jpg",
         scores: [1, 1, 2, 5, 5, 1, 5, 1, 5, 5]
     },
 
